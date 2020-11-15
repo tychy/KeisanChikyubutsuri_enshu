82047db5193c93f8443933b0fae62c16c6d1cd99
Ryu Yamada <a2sin2a2ko@kyoto-y.work>
2020-11-15 18:01:45 +0900

    update : ログの体裁を整えた

--- gauss_jordan_elimination.py
+++ gauss_jordan_elimination.py
@@ -4,6 +4,9 @@ import numpy as np
 def execute(A, b):
     assert A.shape[0] == A.shape[1], "A must be n * n"
     assert A.shape[0] == b.shape[0], "size of A and b must match"
+    print("A:", A)
+    print("b:", b)
+
     for i in range(1, A.shape[0]):
         for j in range(i, A.shape[0]):
             coef = A[j][i - 1] / A[i - 1][i - 1]
@@ -15,7 +18,27 @@ def execute(A, b):
         for j in range(i, A.shape[0] - 1):
             x[i] -= A[i][j + 1] * x[j + 1]
         x[i] /= A[i][i]
+    print("x:", x)
     return x
+
+
+def single(A, b):
+    print("-----single-----")
+    A_copy = np.copy(A).astype(np.float32)
+    b_copy = np.copy(b).astype(np.float32)
+    x = execute(A_copy, b_copy)
+    print("A * x:", A @ x.T)
+    print("-----END-----")
+
+
+def double(A, b):
+    print("double")
+
+    A_copy = np.copy(A).astype(np.float64)
+    b_copy = np.copy(b).astype(np.float64)
+    x = execute(A_copy, b_copy)
+    print("A * x:", A @ x.T)
+    print("-----END-----")
 
 
 if __name__ == "__main__":
@@ -26,11 +49,11 @@ if __name__ == "__main__":
             [4.0, 5.0, 10.0, 7.0],
             [0.0, -1.0, 7.0, 9.0],
         ],
-        dtype=np.float64,
     )
-    A_copy = np.copy(A)
-    b = np.array([16.0, 16.0, 25.0, 16.0], dtype=np.float64)
-    x = execute(A_copy, b)
-    print(x.dtype)
-    print(A_copy.dtype)
-    print(A @ x.T)
+    b = np.array([15.0, 15.0, 25.0, 15.0])
+    c = np.array([16.0, 16.0, 25.0, 16.0])
+
+    single(A, b)
+    double(A, b)
+    single(A, c)
+    double(A, c)

2ef300fb92e2dac7b44d16b5d544a55e05674a46
Ryu Yamada <a2sin2a2ko@kyoto-y.work>
2020-11-15 21:34:45 +0900

    add : scalingを実装

--- gauss_jordan_elimination.py
+++ gauss_jordan_elimination.py
@@ -1,15 +1,30 @@
 import numpy as np
 
 
-def pivot(A, idx):
+def scale(A, x):
+    for i in range(A.shape[0]):
+        max_idx = 0
+        for j in range(A.shape[0]):
+            if np.abs(A[i, j]) > np.abs(A[i, max_idx]):
+                max_idx = j
+        x[i] = x[i] / np.abs(A[i, max_idx])
+        A[i, :] = A[i, :] / np.abs(A[i, max_idx])
+
+    return
+
+
+def pivot(A, x, idx):
     max_idx = idx
     for i in range(idx, A.shape[0]):
         if A[idx, i] > A[idx, max_idx]:
             max_idx = i
     if max_idx != idx:
-        buff = np.copy(A[:, max_idx])
-        A[:, max_idx] = A[:, idx]
-        A[:, idx] = buff
+        buff = np.copy(A[max_idx, :])
+        A[max_idx, :] = A[idx, :]
+        A[idx, :] = buff
+        buffx = x[max_idx]
+        x[max_idx] = x[idx]
+        x[idx] = buffx
     return
 
 
@@ -18,9 +33,10 @@ def execute(A, b):
     assert A.shape[0] == b.shape[0], "size of A and b must match"
     print("A:", A)
     print("b:", b)
-    pivot(A, 0)
+    scale(A, b)
+    pivot(A, b, 0)
     for i in range(1, A.shape[0]):
-        pivot(A, i)
+        pivot(A, b, i)
         for j in range(i, A.shape[0]):
             coef = A[j][i - 1] / A[i - 1][i - 1]
             A[j, :] = A[j, :] - A[i - 1, :] * coef
--- test_gauss_jordan_elimintation.py
+++ test_gauss_jordan_elimintation.py
@@ -7,22 +7,22 @@ class TestGaussJordanElimination(unittest.TestCase):
     def test_pivot(self):
         A = np.array(
             [
-                [10.0, 11.0, 4.0, 0.0],
-                [1.0, 10.0, 5.0, -1.0],
+                [1.0, 11.0, 4.0, 0.0],
+                [10.0, 10.0, 5.0, -1.0],
                 [4.0, 5.0, 10.0, 7.0],
                 [0.0, -1.0, 7.0, 9.0],
             ],
         )
         A_pivot = np.array(
             [
-                [11.0, 10.0, 4.0, 0.0],
-                [10.0, 1.0, 5.0, -1.0],
-                [5.0, 4.0, 10.0, 7.0],
-                [-1.0, 0.0, 7.0, 9.0],
+                [10.0, 10.0, 5.0, -1.0],
+                [1.0, 11.0, 4.0, 0.0],
+                [4.0, 5.0, 10.0, 7.0],
+                [0.0, -1.0, 7.0, 9.0],
             ],
         )
 
-        pivot(A, 0)
+        pivot(A, np.zeros(A.shape[0]), 0)
         self.assertTrue(np.array_equal(A, A_pivot))
 
     def test_execute(self):
@@ -36,6 +36,9 @@ class TestGaussJordanElimination(unittest.TestCase):
         )
         b = np.array([15.0, 15.0, 25.0, 15.0])
         x = execute(np.copy(A), np.copy(b))
+        print(A)
+        print(x)
+        print(A @ x.T)
         self.assertTrue((np.abs(b - A @ x.T) < 0.001).all())
 
 

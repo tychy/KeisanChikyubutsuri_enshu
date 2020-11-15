c48a4e684df1c327272e7cc1522734da45ac8309
Ryu Yamada <a2sin2a2ko@kyoto-y.work>
2020-11-16 00:18:04 +0900

    add : scalingの実装を修正

--- gauss_jordan_elimination.py
+++ gauss_jordan_elimination.py
@@ -2,15 +2,16 @@ import numpy as np
 
 
 def scale(A, x):
+    scale_ls = np.zeros_like(x)
     for i in range(A.shape[0]):
         max_idx = 0
         for j in range(A.shape[0]):
             if np.abs(A[i, j]) > np.abs(A[i, max_idx]):
                 max_idx = j
+        scale_ls[i] = np.abs(A[i, max_idx])
         x[i] = x[i] / np.abs(A[i, max_idx])
         A[i, :] = A[i, :] / np.abs(A[i, max_idx])
-
-    return
+    return scale_ls
 
 
 def pivot(A, x, idx):
@@ -33,7 +34,12 @@ def execute(A, b):
     assert A.shape[0] == b.shape[0], "size of A and b must match"
     print("A:", A)
     print("b:", b)
-    scale(A, b)
+    L = np.zeros_like(A)
+    for i in range(A.shape[0]):
+        L[i, i] = 1
+    scale_ls = scale(A, b)
+    print("Scaled A:", A)
+
     pivot(A, b, 0)
     for i in range(1, A.shape[0]):
         pivot(A, b, i)
@@ -41,22 +47,26 @@ def execute(A, b):
             coef = A[j][i - 1] / A[i - 1][i - 1]
             A[j, :] = A[j, :] - A[i - 1, :] * coef
             b[j] = b[j] - b[i - 1] * coef
+            L[j, i - 1] = coef
     x = np.zeros_like(b)
-    print(A)
+    print("L:", L)
     for i in range(A.shape[0] - 1, -1, -1):
         x[i] += b[i]
         for j in range(i, A.shape[0] - 1):
             x[i] -= A[i][j + 1] * x[j + 1]
         x[i] /= A[i][i]
     print("x:", x)
-    return x
+    for i in range(A.shape[0]):
+        A[i, :] = A[i, :] * scale_ls[i]
+
+    return x, L, A
 
 
 def single(A, b):
     print("-----single-----")
     A_copy = np.copy(A).astype(np.float32)
     b_copy = np.copy(b).astype(np.float32)
-    x = execute(A_copy, b_copy)
+    x, L, U = execute(A_copy, b_copy)
     print("A * x:", A @ x.T)
     print("-----END-----")
 
@@ -66,7 +76,7 @@ def double(A, b):
 
     A_copy = np.copy(A).astype(np.float64)
     b_copy = np.copy(b).astype(np.float64)
-    x = execute(A_copy, b_copy)
+    x, L, U = execute(A_copy, b_copy)
     print("A * x:", A @ x.T)
     print("-----END-----")
 
@@ -80,7 +90,7 @@ if __name__ == "__main__":
             [0.0, -1.0, 7.0, 9.0],
         ],
     )
-    b = np.array([15.0, 15.0, 25.0, 15.0])
+    b = np.array([15.0, 15.0, 26.0, 15.0])
     c = np.array([16.0, 16.0, 25.0, 16.0])
 
     single(A, b)

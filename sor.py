41b9a5abc943ddf3a98693463745569aabe6de94
Ryu Yamada <a2sin2a2ko@kyoto-y.work>
2020-11-24 18:16:00 +0900

    add : omegaを調整

--- sor.py
+++ sor.py
@@ -1,4 +1,6 @@
 import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 
 def split_matrix(A):
@@ -21,7 +23,7 @@ def split_matrix(A):
     return D, L, U
 
 
-def execute(A, b, w):
+def execute(A, b, w, x_true):
     assert A.shape[0] == A.shape[1], "A must be n * n"
     assert A.shape[0] == b.shape[0], "size of A and b must match"
     print("A:", A)
@@ -29,7 +31,16 @@ def execute(A, b, w):
     # D, L, U = split_matrix(A)
     x_prev = np.zeros_like(b)
     x = np.zeros_like(b)
-    while True:
+    step_ls = []
+    x_ls = []
+    step = 0
+    max_step = 300
+    while step <= max_step:
+        if np.linalg.norm(x - x_true) > np.power(10, 10):
+            break
+        if step % 10 == 0:
+            step_ls.append(step)
+            x_ls.append(np.log(np.linalg.norm(x - x_true)))
         x_prev = np.copy(x)
         for i in range(A.shape[0]):
             mida = 0
@@ -41,22 +52,24 @@ def execute(A, b, w):
             x[i] = (
                 -w * mida + (1 - w) * A[i, i] * x_prev[i] - w * midb + w * b[i]
             ) / A[i, i]
-        if (np.abs(x - x_prev) < 0.001).all():
-            break
-    return x
+        step += 1
+    print("x:", x)
+    return x, step_ls, x_ls
 
 
-def double(A, b, w):
+def double(A, b, w, x_true):
     print("-----double-----")
     print("omega:", w)
     A_copy = np.copy(A).astype(np.float64)
     b_copy = np.copy(b).astype(np.float64)
-    x = execute(A_copy, b_copy, w)
+    x, step_ls, x_ls = execute(A_copy, b_copy, w, x_true)
     print("A * x:", A @ x.T)
     print("-----END-----")
+    return step_ls, x_ls
 
 
-if __name__ == "__main__":
+def plot_sor():
+    sns.set_theme()
     A = np.array(
         [
             [1.0, -1.0, 0.0, 0.0],
@@ -66,5 +79,45 @@ if __name__ == "__main__":
         ],
     )
     b = np.array([1.0, 0.0, 0.0, 0.0])
-    w = 0.7
-    double(A, b, w)
+    x_true = np.array(
+        [0.72727272727273, -0.27272727272727, -0.18181818181818, -0.090909090909091]
+    )
+    fig = plt.figure(figsize=(10, 10))
+    for w in [0.1, 0.3, 0.5, 0.7, 0.9, 0.99, 1.0, 1.01, 1.1, 1.3, 1.5, 1.7, 1.9, 3, 5]:
+        step_ls, x_ls = double(A, b, w, x_true)
+        plt.plot(step_ls, x_ls, label="w:{:.2f}".format(w))
+
+    plt.xlabel("step")
+    plt.ylabel("ln|x-x_t|")
+    plt.legend()
+    plt.savefig("sor.png")
+
+
+def plot_sor_debug():
+    sns.set_theme()
+    A = np.array(
+        [
+            [10.0, 1.0, 4.0, 0.0],
+            [1.0, 10.0, 5.0, -1.0],
+            [4.0, 5.0, 10.0, 7.0],
+            [0.0, -1.0, 7.0, 9.0],
+        ],
+    )
+    b = np.array([15.0, 15.0, 26.0, 15.0])
+
+    x_true = np.array([1, 1, 1, 1])
+    fig = plt.figure(figsize=(10, 10))
+
+    for w in [0.1, 0.7, 1.0, 1.1, 1.5, 1.7, 1.8, 1.9]:
+        step_ls, x_ls = double(A, b, w, x_true)
+        plt.plot(step_ls, x_ls, label="w:{:.1f}".format(w))
+
+    plt.xlabel("step")
+    plt.ylabel("ln|x-x_t|")
+    plt.legend()
+    plt.savefig("sor_debug.png")
+
+
+if __name__ == "__main__":
+    plot_sor()
+    plot_sor_debug()

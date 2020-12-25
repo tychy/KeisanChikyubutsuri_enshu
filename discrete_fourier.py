cf9a0da62462cd5cac5a9755c43ffb9df83d7651
Ryu Yamada <a2sin2a2ko@kyoto-y.work>
2020-12-25 11:42:29 +0900

    add : dftの誤差を表示

--- discrete_fourier.py
+++ discrete_fourier.py
@@ -1,10 +1,23 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+from tqdm import tqdm
 
 
-def gen_data(n):
-    arr = np.random.rand(n)
+def gen_data(n, case):
+    if case == 1:
+        arr = np.random.rand(n)
+    else:
+        arr = []
+        theta = np.linspace(0, 2 * np.pi, n)
+        for item in theta:
+            if item <= np.pi:
+                value = -1 + 2 * item / np.pi
+            else:
+                value = 3 - 2 * item / np.pi
+            arr.append(value)
+        arr = np.array(arr)
+
     return arr
 
 
@@ -16,6 +29,7 @@ def coef(f_ls):
         for idx, item in enumerate(f_ls):
             c_k += item * np.exp(-1.0j * 2 * np.pi * i * idx / size)
         coef_ls.append(c_k / size)
+    coef_ls = np.array(coef_ls)
     return coef_ls
 
 
@@ -26,17 +40,49 @@ def dft(coef_ls):
         f_n = 0
         for idx, item in enumerate(coef_ls):
             f_n += coef_ls[idx] * np.exp(1.0j * 2 * np.pi * i * idx / size)
-        f_ls.append(np.real(f_n / size))
+        f_ls.append(np.real(f_n))
     return f_ls
 
 
-def test_dft():
-    arr = gen_data(10)
-    print("input", arr)
-    coef_ls = coef(arr)
-    f_ls = dft(coef_ls)
-    print("out", f_ls)
+def test_dft(n, case):
+    if case == 1:
+        arr = gen_data(n, case)
+        coef_ls = coef(arr)
+        f_ls = dft(coef_ls)
+        print("input:", arr)
+        print("fourier:", f_ls)
+    else:
+        sns.set_theme()
+
+        error_ls = []
+        step_ls = []
+        for idx in tqdm(range(1, n)):
+            arr = gen_data(idx, case)
+            coef_ls = coef(arr)
+            f_ls = dft(coef_ls)
+
+            c_true = np.zeros_like(coef_ls)
+            for i in range(1, idx):
+                c_true[i] = 2 / np.pi / np.pi / i / i * ((-1) ** i - 1)
+            diff = coef_ls - c_true
+            error_ls.append(np.mean(np.sqrt(diff.real ** 2 + diff.imag ** 2)))
+            step_ls.append(idx)
+        # draw
+        step_ls = np.array(step_ls)
+        error_ls = np.array(error_ls)
+        figure = plt.figure()
+        ax1 = figure.add_subplot(1, 2, 1)
+        ax1.set_xlabel("N")
+        ax1.set_ylabel("f(t) - f_N(t)")
+        ax1.plot(step_ls, error_ls)
+
+        ax2 = figure.add_subplot(1, 2, 2)
+        ax2.plot(np.log(step_ls), np.log(error_ls))
+        ax2.set_xlabel("log(N)")
+        ax2.set_ylabel("log(f(t) - f_N(t))")
+        figure.tight_layout()
+        figure.savefig("dft_error.png")
 
 
 if __name__ == "__main__":
-    test_dft()
+    test_dft(100, 2)

8543a9edf1949bb985799ce7587007813e825b4b
Ryu Yamada <a2sin2a2ko@kyoto-y.work>
2021-01-16 17:59:32 +0900

    add : 効率的なRK4

--- ode_pde/runge_kutta.py
+++ ode_pde/runge_kutta.py
@@ -4,7 +4,7 @@ import seaborn as sns
 
 
 def f(x, t):
-    res = x + (x ** 2 + t ** 2) ** 0.5
+    res = x + np.sqrt(x ** 2 + t ** 2)
     res = res / t
     return res
 
@@ -12,7 +12,7 @@ def f(x, t):
 def runge_kutta_one(dh, t_end):
     t = 1
     x = 0
-    while t <= t_end:
+    while t < t_end:
         x = x + dh * f(x, t)
         t += dh
     return x
@@ -21,7 +21,7 @@ def runge_kutta_one(dh, t_end):
 def runge_kutta_two(dh, t_end):
     t = 1
     x = 0
-    while t <= t_end:
+    while t < t_end:
         k_one = dh * f(x, t)
         # Heun法を採用
         k_two = dh * f(x + k_one / 2, t + dh / 2)
@@ -33,12 +33,29 @@ def runge_kutta_two(dh, t_end):
 def runge_kutta_four(dh, t_end):
     t = 1
     x = 0
-    while t <= t_end:
+    while t < t_end:
         k_one = dh * f(x, t)
         k_two = dh * f(x + k_one / 2, t + dh / 2)
         k_three = dh * f(x + k_two / 2, t + dh / 2)
         k_four = dh * f(x + k_three, t + dh)
         x = x + k_one / 6 + k_two / 3 + k_three / 3 + k_four / 6
+        t += dh
+    return x
+
+
+def runge_kutta_four_efficient(dh, t_end):
+    t = 1
+    x = 0
+    while t < t_end:
+        x_cur = x
+        k = dh * f(x, t)
+        x += k / 6
+        k = dh * f(x_cur + k / 2, t + dh / 2)
+        x += k / 3
+        k = dh * f(x_cur + k / 2, t + dh / 2)
+        x += k / 3
+        k = dh * f(x_cur + k, t + dh)
+        x += k / 6
         t += dh
     return x
 
@@ -51,17 +68,31 @@ def plot_runge_kutta():
     two_ls = []
     four_ls = []
     ans = 60
-    dh_ls = [0.0001, 0.0005, 0.001, 0.005, 0.01, 0.05, 0.1, 0.2, 0.5, 1]
+    dh_ls = [
+        0.0001,
+        0.0003,
+        0.0007,
+        0.001,
+        0.003,
+        0.007,
+        0.01,
+        0.03,
+        0.07,
+        0.1,
+        0.2,
+        0.5,
+        1,
+    ]
     for dh in dh_ls:
         one_ls.append(np.fabs(ans - runge_kutta_one(dh, tend)))
         two_ls.append(np.fabs(ans - runge_kutta_two(dh, tend)))
-        four_ls.append(np.fabs(ans - runge_kutta_four(dh, tend)))
+        four_ls.append(np.fabs(ans - runge_kutta_four_efficient(dh, tend)))
     log_dh = np.log10(dh_ls)
-    plt.plot(log_dh, one_ls, label="RK1")
-    plt.plot(log_dh, two_ls, label="RK2")
-    plt.plot(log_dh, four_ls, label="RK4")
-    plt.xlabel("np.log10(h)")
-    plt.ylabel("|60 - x|")
+    plt.plot(log_dh, np.log10(one_ls), label="RK1")
+    plt.plot(log_dh, np.log10(two_ls), label="RK2")
+    plt.plot(log_dh, np.log10(four_ls), label="RK4")
+    plt.xlabel("log10(h)")
+    plt.ylabel("log10(|60 - x|)")
     plt.legend()
     plt.savefig("runge_kutta_error.png")
 
